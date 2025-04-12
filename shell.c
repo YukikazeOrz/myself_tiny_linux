4e71bfc6ad4c6f1cb017c0c558c6c1d329e24691
yuwei.gao <yuwei.gao@thundersoft.com>
2025-04-12 20:32:16 +0800

    modify shell.c

--- shell.c
+++ shell.c
@@ -1,9 +1,19 @@
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(){
     char command[255];
-    write(1, "# ", 2);
-    int count = read(0, command, 255);
-    command[count - 1] = '\0'; // Replace newline with null terminator
-    execve(command, 0, 0);
+    for(;;){write(1, "# ", 2);
+        int count = read(0, command, 255);
+        command[count - 1] = '\0'; // Replace newline with null terminator
+        pid_t fork_result = fork();
+        if (fork_result == 0) {
+            execve(command, 0, 0);
+            break;
+        }else{
+            siginfo_t info;
+            waitid(P_ALL, 0, &info, WEXITED);
+        }
+        
+    }
 }
